527f7014bfdbcc553e067a545a6303f7093f6081
akinyiliz <lizzieke84@gmail.com>
2023-02-22 14:02:42 +0300

    Completed task 3

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -1,16 +1,16 @@
-import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
+import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
 
-test('returns current year', () => {
-	expect(getFullYear()).toBe(2022);
+test("returns current year", () => {
+	expect(getFullYear()).toBe(2023);
 });
 
-test('correct footer copy', () => {
-	expect(getFooterCopy(true)).toBe('Holberton School');
-	expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+test("correct footer copy", () => {
+	expect(getFooterCopy(true)).toBe("Holberton School");
+	expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
 });
 
-test('returns right notification', () => {
+test("returns right notification", () => {
 	expect(getLatestNotification()).toBe(
-		'<strong>Urgent requirement</strong> - complete by EOD'
+		"<strong>Urgent requirement</strong> - complete by EOD"
 	);
 });
